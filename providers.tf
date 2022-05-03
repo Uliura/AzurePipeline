73735cf1d79699eb051769515ccd566af2d4f28c
Evgen Uliura <testuser@ukr.net>
2022-05-03 16:51:22 +0300

    assa

--- providers.tf
+++ providers.tf
@@ -1,4 +1,8 @@
 provider "azurerm" {
+  subscription_id = "${TF_VAR_SUBSCREPTION_ID}"
+  client_id       = "${TF_VAR_CLIENT_ID}"
+  client_secret   = "${TF_VAR_CLIENT_SECRET}"
+  tenant_id       = "${TF_VAR_TENANT_ID}"
   features {}
 }
 
