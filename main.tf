bfa314cc45b8f82caeb0af56bc84325ad7b0f0c3
Evgen Uliura <testuser@ukr.net>
2022-05-03 14:33:01 +0300

    zzz

--- main.tf
+++ main.tf
@@ -5,6 +5,10 @@
 #   tenant_id       = "REPLACE-WITH-YOUR-TENANT-ID"
 # }
 
+terraform {
+   backend "local" {}
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "${var.resource_group_name}"
   location = "${var.location}"
