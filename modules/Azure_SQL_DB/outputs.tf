f404ef5e951666b4b7fabb5ec18ac09a5f3ad45c
Evgen Uliura <testuser@ukr.net>
2022-05-14 12:42:37 +0300

    fix

--- modules/Azure_SQL_DB/outputs.tf
+++ modules/Azure_SQL_DB/outputs.tf
@@ -1,21 +1,21 @@
 output "database_name" {
   description = "Database name of the Azure SQL Database created."
-  value       = "${azurerm_mssql_database.db.name}"
+  value       = azurerm_mssql_database.db.name
 }
 
 output "sql_server_name" {
   description = "Server name of the Azure SQL Database created."
-  value       = "${azurerm_mssql_server.server.name}"
+  value       = azurerm_mssql_server.server.name
 }
 
 output "sql_server_version" {
   description = "Version the Azure SQL Database created."
-  value       = "${azurerm_mssql_server.server.version}"
+  value       = azurerm_mssql_server.server.version
 }
 
 output "sql_server_fqdn" {
   description = "Fully Qualified Domain Name (FQDN) of the Azure SQL Database created."
-  value       = "${azurerm_mssql_server.server.fully_qualified_domain_name}"
+  value       = azurerm_mssql_server.server.fully_qualified_domain_name
 }
 
 output "connection_string" {
